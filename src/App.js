d48d473464ce52dc3877f88d48cf441a42784aff
tatianasnook <tatsnook@gmail.com>
2024-01-11 11:20:13 -0600

    router and links

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './Home';
+import Products from './Products';
+import AboutUs from './AboutUs';
+import Reviews from './Reviews';
+import basket from './basket.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav>
+        <Link to="/" className='link'>Home</Link>
+        <Link to="/products" className='link'>Products</Link>
+        <Link to="/about" className='link'>About Us</Link>
+        <Link to="/reviews" className='link'>Reviews</Link>
+        <img src={basket} alt='basket'/>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/products" element={<Products/>} />
+        <Route path="/about" element={<AboutUs/>} />
+        <Route path="/reviews" element={<Reviews/>}/>
+      </Routes>
+    </Router>
   );
 }
 
