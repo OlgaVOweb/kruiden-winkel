5463afcb654601223d11c7ddff64fd76aaa7d85d
Olya2125 <139589012+Olya2125@users.noreply.github.com>
2024-01-31 22:10:54 +0300

    abc

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home/Home';
-
 import Delivery from './pages/Delivery/Delivery';
 import Payment from './pages/Payment';
 import basket from './icons/basket.png';
