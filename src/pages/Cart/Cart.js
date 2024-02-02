0708851c3f8f33915ae2d9705da911c0a6567987
tatianasnook <tatsnook@gmail.com>
2024-02-02 17:10:55 -0600

    added data, remove Payment

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -4,7 +4,8 @@ import './cart.css';
 const Cart = () => {
   return (
     <div className='main'>
-      <h4>This is a Cart page</h4>
+      <h2>Your Cart</h2>
+      <h3>TOTAL: $</h3>
     </div>
   )
 }
