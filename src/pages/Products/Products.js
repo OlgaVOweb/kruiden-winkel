794381c02f350d299aaa1fa35ac39b333c22c8fd
Olya2125 <139589012+Olya2125@users.noreply.github.com>
2024-02-10 20:44:10 +0300

    changed some things

--- src/pages/Products/Products.js
+++ src/pages/Products/Products.js
@@ -1,15 +1,23 @@
 import './products.css';
 import ProductsItem from "./ProductCard/ProductsItem";
 import AllCategories from "./Filter/AllCategories";
+import React, { useState } from 'react';
 
 function ProductsPage(){
+
+  const [showFilter, setShowFilter] = useState(false);
+
+  const buttonClick = () => {
+    setShowFilter(!showFilter);
+  };
+
     return (
       <div className="container">
         <div className="container__header">
           <h2 className="container__header-products">PRODUCTS</h2>
-          <button className="container__header-button">FILTER</button>
-        </div>
-        <AllCategories />
+          <button className="container__header-button" onClick={buttonClick}>FILTER</button>
+          </div>
+          {showFilter && <AllCategories/>}
         <ProductsItem  />
       </div>
     );
