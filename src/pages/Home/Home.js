0176ea3cf531d80b2f9b1b6baa320c0cb4a81f1f
OlgaVOweb <13928565669+OlgaVOweb@users.noreply.github.com>
2024-02-19 15:30:20 +0100

    changed some in css

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -9,86 +9,78 @@ import Top from '../Top/Top';
 
 
 const Home = () => {
-
-
-
   return (
     <div className='home-page'>
-    <div className="main-home">
-     <img src={tea} className='img-mainn' alt='tea' />
-     <div className='main-text'>
-     <p className='big-text'>TURAN KRUIDEN EN THEE</p>
-     <p className='mini-text  top'>WIJ VERKOOP AL 16 JAAR THEE EN KRUIDEN</p>
+      <div className="main-home">
+        <img src={tea} className='img-mainn' alt='tea' />
+        <div className='main-text'>
+          <p className='big-text'>TURAN KRUIDEN EN THEE</p>
+          <p className='mini-text  top'>WIJ VERKOOP AL 16 JAAR THEE EN KRUIDEN</p>
+          <p className='mini-text'>DAAROM GARANDEREN WIJ DE KWALITEIT!</p>
+        </div>
+        <img src={spice} className='img-mainn' alt='spice' />
+      </div>
 
-<p className='mini-text'>DAAROM GARANDEREN WIJ DE KWALITEIT!</p>
-     </div>
-     <img src={spice} className='img-mainn' alt='spice' />
-
-    </div>
-
-    <div className='block-second'>
+      <div className='block-second'>
         <div className='best-text'>
           <p className='one-text'>Bestsellers</p>
           <p className='two-text'>Legendary products won love our clients</p>
           <button className='btn-bestsellers'>Look</button>
-</div>
+        </div>
 
-<div className='card-best'>
-<div className='card-bestsellers width-mini'>
-<img className="bestsellers-img " src={kerryMasala} alt="prod"/>
-                    <div className="product__card-content">
-                        <h4 className="product__card-header">Kerry Masala, 50g </h4>
-                        <p className="product__card-price">€ 1.75</p>
-                        {/* <button className='more'>Read more</button> */}
-                    </div> 
-</div>
-                                    <div className='card-bestsellers width-mini'>
-                    <img className="bestsellers-img " src={kerryMasala} alt="prod"/>
-                    <div className="product__card-content">
-                        <h4 className="product__card-header">Kerry Masala, 50g </h4>
-                        <p className="product__card-price">€ 1.75</p>
-                        {/* <button className='more'>Read more</button> */}
-                    </div>
-                                    </div>
-                                    <div className='card-bestsellers width-mini none'>
-                                    <img className="bestsellers-img " src={kerryMasala} alt="prod"/>
-                    <div className="product__card-content">
-                        <h4 className="product__card-header">Kerry Masala, 50g </h4>
-                        <p className="product__card-price">€ 1.75</p>
-                        {/* <button className='more'>Read more</button> */}
-                    </div>
-                                    </div>
-                                    </div>
-    
-</div>
-
-
-    <div className='block-third'>
-      <div className='block-3'>
-    <img className="banner-img" src={banner1} alt="tea"/>
-    <div className='enjoy-main-text'>
-          <p className='enjoy-text'>Enjoy the variety of flavors of our tea</p>
-          <p className='collection-text'>Our store has the most unique collection of tea so that you can enjoy the beneficial properties and unique tastes.</p>
-          <button className='btn-bestsellers enjoy-btn'>Products</button>
-</div>
-</div>
+        <div className='card-best'>
+          <div className='card-bestsellers width-mini'>
+            <img className="bestsellers-img " src={kerryMasala} alt="prod"/>
+              <div className="product__card-content">
+                <h4 className="product__card-header">Kerry Masala, 50g </h4>
+                <p className="product__card-price">€ 1.75</p>
+               {/* <button className='more'>Read more</button> */}
+          </div> 
+        </div>
+        <div className='card-bestsellers width-mini'>
+          <img className="bestsellers-img " src={kerryMasala} alt="prod"/>
+            <div className="product__card-content">
+              <h4 className="product__card-header">Kerry Masala, 50g </h4>
+              <p className="product__card-price">€ 1.75</p>
+              {/* <button className='more'>Read more</button> */}
+            </div>
+          </div>
+        
+        <div className='card-bestsellers width-mini none'>
+          <img className="bestsellers-img " src={kerryMasala} alt="prod"/>
+            <div className="product__card-content">
+              <h4 className="product__card-header">Kerry Masala, 50g </h4>
+              <p className="product__card-price">€ 1.75</p>
+              {/* <button className='more'>Read more</button> */}
+            </div>
+        </div>
+      </div>
     </div>
+
     <div className='block-third'>
       <div className='block-3'>
-    
-    <div className='range-main-text'>
+        <img className="banner-img" src={banner1} alt="tea"/>
+          <div className='enjoy-main-text'>
+            <p className='enjoy-text'>Enjoy the variety of flavors of our tea</p>
+            <p className='collection-text'>Our store has the most unique collection of tea so that you can enjoy the beneficial properties and unique tastes.</p>
+            <button className='btn-bestsellers enjoy-btn'>Products</button>
+          </div>
+        </div>
+      </div>
+    <div className='block-third'>
+      <div className='block-3'>
+        <div className='range-main-text'>
           <p className='enjoy-text'>A wide range of</p>
           <p className='collection-text-two'>Our wide range of herbs will not leave you without a purchase.
-After all, your dishes will acquire incredible notes and taste.</p>
+          After all, your dishes will acquire incredible notes and taste.</p>
           <button className='btn-bestsellers enjoy-btn'>Products</button>
-</div>
-<img className="banner-img imgg" src={banner2} alt="spice"/>
-</div>
+        </div>
+        <img className="banner-img imgg" src={banner2} alt="spice"/>
+      </div>
     </div>
     <Contacts/>
-<Top/>
+    <Top/>
     </div>
-
   )
 }
 
